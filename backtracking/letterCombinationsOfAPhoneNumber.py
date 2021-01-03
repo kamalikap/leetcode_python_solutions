fe7723bc193b78b66230159c6d375ff3cf5cd6ae
kamalikap <kamalikap22@gmail.com>
2021-01-03 10:10:32 -0800

    added backtracking problems

---
+++ backtracking/letterCombinationsOfAPhoneNumber.py
@@ -0,0 +1,43 @@
+"""
+17. Letter combination of a Phone Number::Runtime: 24 ms, faster than 43.40%
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+Input: digits = ""
+Output: []
+
+Example 3:
+Input: digits = "2"
+Output: ["a","b","c"]
+
+
+"""
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        dic = { "2": "abc", "3": "def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        
+        res=[]
+        if len(digits) ==0:
+            return res
+            
+        self.dfs(digits, 0, dic, '', res)
+        return res
+    
+    def dfs(self, nums, index, dic, path, res):
+        if index >=len(nums):
+            res.append(path)
+            return
+        string1 =dic[nums[index]]
+        for i in string1:
+            self.dfs(nums, index+1, dic, path + i, res)
